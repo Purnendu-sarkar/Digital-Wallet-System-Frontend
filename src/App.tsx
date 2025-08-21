c1c056fbb2d775377f260ab599fbbbf5161deefa
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-08-21 15:34:25 +0600

    Setup React Router with base configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Outlet } from "react-router";
 import { Button } from "./components/ui/button";
-
 
 function App() {
   return (
     <>
-    <h1>Welcome to the Digital Wallet System</h1>
-    <p>Your one-stop solution for managing your digital assets.</p>
-    <Button>Get Started</Button>
+      <h1>Welcome to the Digital Wallet System</h1>
+      <p>Your one-stop solution for managing your digital assets.</p>
+      <Button>Get Started</Button>
+      <Outlet></Outlet>
     </>
   );
 }
