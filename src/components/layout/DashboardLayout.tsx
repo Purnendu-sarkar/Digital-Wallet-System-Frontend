94d2d82747003753dc8b2cc79bbfc9a6a9b41afd
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-09-08 15:19:59 +0600

    update: added UserMenu to DashboardLayout

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -7,19 +7,25 @@ import {
 } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
 import { ModeToggle } from "./ModeToggler";
+import UserMenu from "../user-menu";
+import { useUserInfoQuery } from "@/redux/features/auth/auth.api";
 
 export default function DashboardLayout() {
+  const { data: user } = useUserInfoQuery();
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
-          <ModeToggle />
+
           <Separator
             orientation="vertical"
             className="mr-2 data-[orientation=vertical]:h-4"
           />
+          <div className="flex-1" />
+          <ModeToggle />
+          <UserMenu user={user?.data} />
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
           <Outlet />
